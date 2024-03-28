e7c236d2f4d577c74eee29b976bcbc11679dd3c3
a <antoniomusciano@gmail.com>
2024-03-28 00:12:11 -0400

    this was the silliest thing ive ever created

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import fileinput
+import matplotlib.pyplot as plt
 
 
 def stageAnalyzer():
-    road_pieces = [10, 14, 23, 13, 12, 11, 45, 46]
-    offroad_pieces = [15, 17, 24, 16, 44, 43]
+    road_pieces = [10, 14, 23, 13, 12, 11, 45, 46,47]
+    offroad_pieces = [15, 17, 24, 16, 44, 43,48]
     road = 0
     offroad = 0
+    name = ''
 
     filename = "stage.txt"
 
     for line in fileinput.input(files=filename):
-        if line.startswith("set("):
-            number = int(line.split("(")[1].split(",")[0])  
+        if line.startswith("name"):
+            name = line.split("(")[1].split(")")[0]
+        elif line.startswith("set("):
+            number = int(line.split("(")[1].split(",")[0])
             if number in road_pieces:
                 road += 1
             elif number in offroad_pieces:
                 offroad += 1
             else:
                 pass
-    return road, offroad
+
+    stageGrapher(road,offroad,name)
+
+
+def stageGrapher(r, o, n):
+    # r is road, o is offroad, n is name
+    labels = ['Road', 'Offroad']
+    sizes = [r, o]
+
+    # Create a pie chart
+    plt.figure(figsize=(5,5))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    plt.title(f'Distribution of Road and Offroad Pieces\n for {n}')
+    # plt.axis('equal')
+    plt.show()
 
 
 
